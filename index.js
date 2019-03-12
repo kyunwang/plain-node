0ee6946f98ac786ebff741de647116165b62ebfb
Kangyun Wang <kevinwang8011@hotmail.com>
2019-03-12 21:05:29 +0100

    Simple file stream

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { createServer } = require('http');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 8080;
 
@@ -9,10 +10,13 @@ server.on('request', (req, res) => {
 	// res.end('Hello World!');
 
 	// Steams allow this - we can write in our stream multiple times!
-	res.write('Hello');
-	res.write(',');
-	res.write('World!');
-	res.end();
+	// res.write('Hello');
+	// res.write(',');
+	// res.write('World!');
+	// res.end();
+
+	// Stream piping for res objext and other streams. The piped stream will end automatically when the original one ends
+	fs.createReadStream(__filename).pipe(res);
 });
 
 server.listen(PORT, () => {
