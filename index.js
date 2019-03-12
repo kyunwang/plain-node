babe66caa59caa552c40e0310229cc7f6ec52e36
Kangyun Wang <kevinwang8011@hotmail.com>
2019-03-12 21:13:15 +0100

    Simple routing

--- index.js
+++ index.js
@@ -5,29 +5,36 @@ const PORT = process.env.PORT || 8080;
 
 const server = createServer();
 
+// Node handles all request by a single request handler
 server.on('request', (req, res) => {
-	// Most simple hello world
-	// res.end('Hello World!');
-
-	// Steams allow this - we can write in our stream multiple times!
-	// res.write('Hello');
-	// res.write(',');
-	// res.write('World!');
-	// res.end();
-
-	// Stream piping for res objext and other streams. The piped stream will end automatically when the original one ends
-	// fs.createReadStream(__filename).pipe(res);
-
-	// Setting headers
-	// res.setHeader('content-type', 'application/json');
-	// plain object cannot be send, instead buffers or strings will do
-	// res.end(JSON.stringify({ data: 42 }));
-
-	// Setting statuscodes - two ways
-	res.writeHead(204, 'Oops no content here.');
-	// res.statusCode = 204;
-	// res.statucMessage = 'Content is not found';
-	res.end();
+	switch (req.url) {
+		case '/':
+			// Most simple hello world
+			res.end('Hello World!');
+			break;
+		case '/stream':
+			// Steams allow this - we can write in our stream multiple times!
+			res.write('Hello');
+			res.write(',');
+			res.write('World!');
+			res.end();
+			break;
+		case '/stream-fs':
+			// Stream piping for res objext and other streams. The piped stream will end automatically when the original one ends
+			fs.createReadStream(__filename).pipe(res);
+			break;
+		case '/header':
+			// Setting headers
+			res.setHeader('content-type', 'application/json');
+			// plain object cannot be send, instead buffers or strings will do
+			res.end(JSON.stringify({ data: 42 }));
+		case '/status':
+			// Setting statuscodes - two ways
+			res.writeHead(204, 'Oops no content here.');
+			// res.statusCode = 204;
+			// res.statucMessage = 'Content is not found';
+			res.end();
+	}
 });
 
 server.listen(PORT, () => {
